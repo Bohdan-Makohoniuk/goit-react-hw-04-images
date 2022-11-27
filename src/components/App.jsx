6f9fbaa2cba123510e63c71f3c4c2c018b5c75bd
Bohdan <makohoniuk.bo@gmail.com>
2022-11-27 13:39:55 +0200

    рефактор_хуки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import { fetchImages } from './api/api';
 import Searchbar from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -10,71 +10,131 @@ import { Modal } from './Modal/Modal';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export class App extends Component {
-  state = {
-    images: [],
-    query: '',
-    page: 1,
-    isLoading: false,
-    modalImage: '',
-    showModal: false,
-    totalHits: 0,
-    showBtn:false,
-  };
+export default function App() {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [modalImage, setModalImage] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [totalHits, setTotalHits] = useState(0);
+  const [showBtn, setShowBtn] = useState(false);
 
-  componentDidUpdate = (_, prevState) => {
-    if (
-      this.state.query !== prevState.query ||
-      this.state.page !== prevState.page
-    ) {
-      this.setState({ isLoading: true });
-      fetchImages(this.state.query, this.state.page)
+  useEffect(() => {
+      if (!query) {
+        return;
+      }
+        setIsLoading(true);
+        fetchImages(query, page)
         .then(data => {
-          this.setState(prevState => ({
-            
-            images: [...prevState.images, ...data.hits],
-            showBtn: this.state.page < Math.ceil(data.totalHits / 12),
-          }));
+            setImages(totalHits => (page === 1 ? [...data.totalHits] : [...images, ...data.totalHits]));
+            setTotalHits(totalHits => (page === 1 ? totalHits - data.totalHits.length : totalHits - [...data.totalHits].length));
         })
         .finally(() => {
-          this.setState({ isLoading: false });
-        });
-    }
+            setIsLoading(false);
+        })
+    }, [query, page,images]);
+
+
+
+  const handleSubmit = query => {
+    setQuery('');
+    setPage(1);
+    setImages([]);
   };
 
-  handleSubmit = query => {
-    this.setState({ query, page: 1, images: [] });
+  const handleLoadMore = () => {
+    setPage(page + 1);
   };
 
-  handleLoadMore = () => {
-    this.setState(state => ({ page: state.page + 1 }));
+  const toggleModalimg = () => {
+    setShowModal(!showModal);
   };
 
-  toggleModal = modalImage => {
-    if (!modalImage) {
-      this.setState({ modalImage: '', showModal: false });
-      return;
-    }
-    this.setState({ modalImage, showModal: true });
+  const toggleModal = ({ modalImage }) => {
+    setModalImage({ ...modalImage });
+    toggleModalimg();
   };
 
-  render() {
-    return (
-      <>
-        <Searchbar onSubmit={this.handleSubmit} />
-        {this.state.isLoading && <Loader />}
-        <ImageGallery images={this.state.images} openModal={this.toggleModal} />
+  return (
+    <>
+      <Searchbar onSubmit={handleSubmit} />
+      {isLoading && <Loader />}
+      <ImageGallery images={images} openModal={toggleModal} />
 
-        {this.state.showBtn && <LoadMore onLoadMore={this.handleLoadMore} />}
+      {showBtn && <LoadMore onLoadMore={handleLoadMore} />}
 
-        {this.state.showModal && (
-          <Modal
-            modalImage={this.state.modalImage}
-            closeModal={this.toggleModal}
-          />
-        )}
-        <ToastContainer autoClose={1000} />
-      </>
-    );
-  }
+      {showModal && <Modal modalImage={modalImage} closeModal={toggleModal} />}
+      <ToastContainer autoClose={1000} />
+    </>
+  );
 }
+
+// export class App extends Component {
+//   state = {
+//     images: [],
+//     query: '',
+//     page: 1,
+//     isLoading: false,
+//     modalImage: '',
+//     showModal: false,
+//     totalHits: 0,
+//     showBtn:false,
+//   };
+
+  // componentDidUpdate = (_, prevState) => {
+  //   if (
+  //     this.state.query !== prevState.query ||
+  //     this.state.page !== prevState.page
+  //   ) {
+  //     this.setState({ isLoading: true });
+  //     fetchImages(this.state.query, this.state.page)
+  //       .then(data => {
+  //         this.setState(prevState => ({
+
+  //           images: [...prevState.images, ...data.hits],
+  //           showBtn: this.state.page < Math.ceil(data.totalHits / 12),
+  //         }));
+  //       })
+  //       .finally(() => {
+  //         this.setState({ isLoading: false });
+  //       });
+  //   }
+  // };
+
+// handleSubmit = query => {
+//   this.setState({ query, page: 1, images: [] });
+// };
+
+// handleLoadMore = () => {
+//   this.setState(state => ({ page: state.page + 1 }));
+// };
+
+// toggleModal = modalImage => {
+//   if (!modalImage) {
+//     this.setState({ modalImage: '', showModal: false });
+//     return;
+//   }
+//   this.setState({ modalImage, showModal: true });
+// };
+
+//   render() {
+// return (
+//   <>
+//     <Searchbar onSubmit={this.handleSubmit} />
+//     {this.state.isLoading && <Loader />}
+//     <ImageGallery images={this.state.images} openModal={this.toggleModal} />
+
+//     {this.state.showBtn && <LoadMore onLoadMore={this.handleLoadMore} />}
+
+//     {this.state.showModal && (
+//       <Modal
+        // modalImage={this.state.modalImage}
+//         closeModal={this.toggleModal}
+//       />
+//     )}
+//     <ToastContainer autoClose={1000} />
+//   </>
+// );
+//   }
+// }
